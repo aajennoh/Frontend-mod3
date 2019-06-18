2ef06f0c2edd30dd728a866a60a8e54beeca5da3
Jennifer Oh <ohjennifer.90@gmail.com>
2019-06-18 14:41:04 -0400

    using cloudinary

--- src/index.js
+++ src/index.js
@@ -77,3 +77,32 @@ function renderCard(){
             </div>
         </div>`
 }
+
+const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/aajennoh/upload'
+const CLOUDINARY_UPLOAD_PRESET = 'd02xpqpf'
+let fileUpload = document.querySelector('#file-upload')
+let submitForm = document.querySelector('#submit-form')
+let currentFile = null
+fileUpload.addEventListener('change', function(event){
+    currentFile = event
+})
+
+submitForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let file = currentFile.target.files[0];
+    let formData = new FormData();
+    formData.append('file', file)
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+    axios({
+        url: CLOUDINARY_URL,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: formData
+    }).then(function(response){
+        console.log(response)
+    }).catch(function(error) {
+        console.error(error)
+    })
+})
