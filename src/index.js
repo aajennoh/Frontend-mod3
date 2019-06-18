e4d9b0e789d558bfbb10a01413291e2d491ea813
Carly Pecora <carly@carlypecora.com>
2019-06-18 17:13:52 -0400

    add login

--- src/index.js
+++ src/index.js
@@ -1,67 +1,78 @@
+//DECLARATIONS & ASSIGNMENTS
 var map;
 var locations = [
     {lat: 1, lng: 1},
     {lat: -43.99972, lng: 170.463352}
 ]
-let currentLat = null
-let currentLong = null
+let currentUser = null;
+let currentUsername = null;
+let currentUseremail = null;
+let currentLat = null;
+let currentLong = null;
+const loginForm = document.querySelector("#login-form")
+const mapAndSubmit = document.querySelector("#logged-in")
+const CLOUDINARY_URL = `${CLOUDINARYURL}`
+const CLOUDINARY_UPLOAD_PRESET = `${PRESET}`
+let fileUpload = document.querySelector('#file-upload')
+let submitForm = document.querySelector('#submit-form')
+let currentFile = null
+const mapDiv = document.querySelector('#map')
+const viewCard = document.querySelector('.w3-card-4')
 
-function initMap() {                            
-    var latitude = 0; // YOUR LATITUDE VALUE
-    var longitude = 0; // YOUR LONGITUDE VALUE
-    var myLatLng = {lat: latitude, lng: longitude};
-    
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 2,
-    disableDoubleClickZoom: true // disable the default map zoom on double click
-    });
-    
-    
-    var markers = locations.forEach(function(location){
-        var newMark = new google.maps.Marker({
-            position: location,
-            map: map,
-            title: location.lat + ', ' + location.lng
-        })
-    })
-
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        // setting latitude & longitude as title of the marker
-        // title is shown when you hover over the marker
-        title: latitude + ', ' + longitude 
-    });
-    
-    // Create new marker on double click event on the map
-    google.maps.event.addListener(map,'dblclick',function(event) {
-        currentLat = event.latLng.lat();
-        currentLong = event.latLng.lng();
-        // console.log(currentLat, currentLong)
-        var marker = new google.maps.Marker({
-            position: event.latLng, 
-            map: map, 
-            title: event.latLng.lat()+', '+event.latLng.lng()
-        }); 
-        $("#submit-form").slideDown();
-        locations.push({lat: marker.position.lat(), lng: marker.position.lng()});
-    });
-}
-
+//CREATE HTML SECRET SCRIPT TAG
 const googleApi = document.createElement('script')
 googleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`
 document.body.appendChild(googleApi)
 
-// event listener for marker click
-const mapDiv = document.querySelector('#map')
-const viewCard = document.querySelector('.w3-card-4')
-mapDiv.addEventListener('click', function(event) {
-    if (event.target.tagName === 'AREA'){
-        renderCard()
-    }
-})
+//MAP INIT
+function initMap() {
+    if(currentUser !== null) { 
+        mapAndSubmit.style.display = "block"
+        loginForm.style.display = "none"
+        var latitude = 0; 
+        var longitude = 0; 
+        var myLatLng = {lat: latitude, lng: longitude};
+        
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: myLatLng,
+            zoom: 2,
+            disableDoubleClickZoom: true 
+        });
+         
+        var markers = locations.forEach(function(location){
+            var newMark = new google.maps.Marker({
+                position: location,
+                map: map,
+                title: location.lat + ', ' + location.lng
+            })
+        })
 
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: latitude + ', ' + longitude 
+        });
+        
+        google.maps.event.addListener(map,'dblclick',function(event) {
+            currentLat = event.latLng.lat();
+            currentLong = event.latLng.lng();
+            var marker = new google.maps.Marker({
+                position: event.latLng, 
+                map: map, 
+                title: event.latLng.lat()+', '+event.latLng.lng()
+            }); 
+            $("#submit-form").slideDown();
+            locations.push({lat: marker.position.lat(), lng: marker.position.lng()});
+            });
+    } else {
+        loginForm.style.display = "block"
+        mapAndSubmit.style.display = "none"
+        console.log("no current user")
+    }
+}
+
+
+//HELPER METHODS
 function renderCard(){
     let cardInnerHTML = document.querySelector('#card')
     cardInnerHTML.innerHTML = ''
@@ -78,11 +89,13 @@ function renderCard(){
         </div>`
 }
 
-const CLOUDINARY_URL = `${CLOUDINARYURL}`
-const CLOUDINARY_UPLOAD_PRESET = `${PRESET}`
-let fileUpload = document.querySelector('#file-upload')
-let submitForm = document.querySelector('#submit-form')
-let currentFile = null
+//EVENT LISTENERS
+mapDiv.addEventListener('click', function(event) {
+    if (event.target.tagName === 'AREA'){
+        renderCard()
+    }
+})
+
 fileUpload.addEventListener('change', function(event){
     currentFile = event
 })
@@ -103,7 +116,9 @@ submitForm.addEventListener('submit', function(event) {
     }).then(sendPhotoData)
 })
 
+loginForm.addEventListener("submit", login)
 
+//FETCHES
 function sendPhotoData(response){
     let responseData = response.data.secure_url
     let data = {'name': responseData};
@@ -118,5 +133,35 @@ function sendPhotoData(response){
     }).catch(function(error) {
         console.error(error)
     })
-
 }
+
+function login(e){
+    e.preventDefault();
+    let username = document.querySelector("#username");
+    let useremail = document.querySelector("#useremail");
+    currentUsername = username.value;
+    currentUseremail = useremail.value;
+
+    fetch("http://localhost:3000/api/v1/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin" : "*", 
+            "Access-Control-Allow-Credentials" : true, 
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: currentUsername, 
+            email: currentUseremail
+        })
+    })
+    .then(res => res.json())
+    .then(json => {
+        currentUser = json;
+        initMap()
+    })
+}
+
+
+
+
